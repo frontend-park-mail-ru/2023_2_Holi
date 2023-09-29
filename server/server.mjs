e7e71b7289b278f8cbca46bac145dac96b5fec8f
maffin1799 <nik.chujko@mail.ru>
2023-09-29 21:25:18 +0300

    pages

--- server/server.mjs
+++ server/server.mjs
@@ -14,6 +14,22 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../pages','main.html'))
 })
 
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '../pages','login.html'))
+})
+
+app.get('/register/create', (req, res) => {
+    res.sendFile(path.join(__dirname, '../pages','register_create_password.html'))
+})
+
+app.get('/register/finish', (req, res) => {
+    res.sendFile(path.join(__dirname, '../pages','register_finish_acc.html'))
+})
+
+app.get('/register/was_created', (req, res) => {
+    res.sendFile(path.join(__dirname, '../pages','register_if_account_alredy_created.html'))
+})
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Сервер запущен на порту ${port}`);
