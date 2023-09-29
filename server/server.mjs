4a0e9927ac25775f451b81d6ddec55071cc59829
maffin1799 <nik.chujko@mail.ru>
2023-09-29 21:33:01 +0300

    fix foulder

--- server/server.mjs
+++ server/server.mjs
@@ -8,7 +8,7 @@ const __dirname = dirname(__filename);
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/pages','main.html'))
