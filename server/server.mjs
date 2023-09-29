22ca051c61d62a135ac328f325d6cc7ab10fca64
maffin1799 <nik.chujko@mail.ru>
2023-09-29 20:56:07 +0300

    server cringe

--- server/server.mjs
+++ server/server.mjs
@@ -1,5 +1,10 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 
@@ -9,7 +14,7 @@ app.use(express.static(path.join(__dirname, 'public/video')));
 app.use(express.static(path.join(__dirname, 'public/fonts')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/pages','main.html'))
+    res.sendFile(path.join(__dirname, '../public/pages','main.html'))
 })
 
 const port = process.env.PORT || 3000;
