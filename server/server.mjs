c0d290579050f60682f7cb565a882676786a2d1f
ShelbyKS <krylov.sanches@mail.ru>
2023-10-02 00:08:33 +0300

    changed port

--- server/server.mjs
+++ server/server.mjs
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
 
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 app.listen(port, () => {
     console.log(`Сервер запущен на порту ${port}`);
 })
