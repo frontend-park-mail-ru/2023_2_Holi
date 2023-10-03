8d481cb2dffb8171c99cd9b64d5f142a844393bd
maffin1799 <nik.chujko@mail.ru>
2023-10-03 19:27:09 +0300

    dramas

--- src/pages/feed/feed.js
+++ src/pages/feed/feed.js
@@ -5,6 +5,7 @@ import { uuid } from "../../services/uuid-time.js";
 import { FeedFooter } from "./components/footer/footer.js";
 import { logoutRequest } from "../../services/api/auth.js";
 import { goToLink } from "../../services/goToLink.js";
+import { getGenreFilms } from "../../services/api/genre.js";
 
 
 const witcherImage = 'The-Witcher-3-season-2022.jpg';
@@ -74,7 +75,8 @@ export class FeedPage {
         dragging(id_carousel, id_container);
     }
 
-    render() {
+    async render() {
+        await getGenreFilms('Dramas')
         this.#parent.innerHTML = '';
         this.#parent.style.background = '';
         const header = document.createElement('div')
@@ -97,6 +99,8 @@ export class FeedPage {
         this.#parent.appendChild(footer);
 
         goToFilms();
+
+       
 
         document.getElementById('logout').addEventListener('click', async function(){
             const response = await logoutRequest();
