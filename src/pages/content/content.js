5133ad2c3050aea0ca6a39b15d3a24923f7e3eac
maffin1799 <nik.chujko@mail.ru>
2023-10-29 23:05:15 +0300

    fixxx

--- src/pages/content/content.js
+++ src/pages/content/content.js
@@ -1,5 +1,6 @@
 import { logoutRequest } from '../../services/api/auth.js';
 import { getContentById } from '../../services/api/content.js';
+import { getLastNumber } from '../../services/getParams.js';
 import { navigate } from '../../services/router/Router.js';
 
 /*global Handlebars */
@@ -13,20 +14,39 @@ export class ContentPage {
         this.#parent.innerHTML = '';
         this.#parent.style.background = '';
         const template = Handlebars.templates['content.hbs'];
-
-        getContentById(5);
+        const id = getLastNumber(location.href);
+        getContentById(id);
         this.#parent.innerHTML = template();
 
-        document.getElementById('logout').addEventListener('click', async function() {
+        document.getElementById('logout').addEventListener('click', async function () {
             const response = await logoutRequest();
             if (response.ok) {
                 navigate('/login');
             }
         });
 
-        document.getElementById('dropdown').addEventListener('click', function() {
+        document.getElementById('dropdown').addEventListener('click', function () {
             this.parentNode.parentNode.classList.toggle('closed');
         }, false);
 
+        videoController();
     }
 }
+
+export const videoController = () => {
+    const video = document.querySelector('.content-container__player');
+
+    // Получаем текущее время воспроизведения из localStorage (если есть)
+    const currentTime = localStorage.getItem(location.href);
+    if (currentTime) {
+        video.currentTime = parseFloat(currentTime);
+    }
+    video.addEventListener('play', () => {
+        video.requestFullscreen();
+    });
+
+    // Сохраняем текущее время воспроизведения при его изменении
+    video.addEventListener('timeupdate', () => {
+        localStorage.setItem(location.href, video.currentTime);
+    });
+};
