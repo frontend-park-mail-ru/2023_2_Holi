ea57be0d584ae108327caa85f73bbc772cffad10
maffin1799 <nik.chujko@mail.ru>
2023-11-13 14:23:50 +0300

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,12 @@ module.exports = {
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['@babel/preset-env'],
+                        presets: [
+                            ['@babel/preset-env', {
+                                useBuiltIns: 'entry', // Включение только необходимых полифилов
+                                corejs: 3, // Версия библиотеки core-js
+                            }],
+                        ],
                     },
                 },
             },
