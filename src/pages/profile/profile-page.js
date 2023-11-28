541331cd40888d5ec1112f6581069c60fbd44f0c
maffin1799 <nik.chujko@mail.ru>
2023-11-28 13:04:34 +0300

    fix

--- src/pages/profile/profile-page.js
+++ src/pages/profile/profile-page.js
@@ -1,8 +1,8 @@
 import { Notify } from '../../components/notify/notify.js';
 import { logoutRequest } from '../../services/api/auth.js';
-import { getUserInfo, setUserInfo } from '../../services/api/user.js';
+import { setUserInfo } from '../../services/api/user.js';
 import { navigate } from '../../services/router/Router.js';
-//import { validatePassword } from '../../services/validate.js';
+import { validatePassword } from '../../services/validate.js';
 import profile from './profile-page.hbs';
 import store from '../../../index.js';
 import { $sentUserInfoRequest, USER_REDUCER } from '../../services/flux/actions/user-info.js';
@@ -14,14 +14,14 @@ export class ProfilePage {
     }
 
     async render() {
-
-        const userInfo = await getUserInfo(localStorage.getItem('userId'));
         this.#parent.innerHTML = '';
         this.#parent.innerHTML = profile();
         const profileForm = document.forms['profile-form'];
         const emailInput = profileForm.elements['email'];
         const passwordInput = profileForm.elements['password'];
         const fileInput = profileForm.elements['file'];
+
+        let confirm = false;
 
         /**
          * Узнаю о пользователе
@@ -93,14 +93,19 @@ export class ProfilePage {
 
         // Добавьте обработчики событий на соответствующие инпуты
 
-        emailInput.addEventListener('change', () => {
+        emailInput.addEventListener('input', () => {
             if (emailInput.value) {
                 formData.email = emailInput.value;
+                confirm = true;
             }
         });
-        passwordInput.addEventListener('change', () => {
-            if (passwordInput.value) {
+        passwordInput.addEventListener('input', () => {
+            if (passwordInput.value && validatePassword(passwordInput.value) === '') {
                 formData.password = Array.from(new TextEncoder().encode(passwordInput.value));
+                confirm = true;
+            } else {
+                new Notify(validatePassword(passwordInput.value));
+                confirm = false;
             }
         });
         // fileInput.addEventListener('change', () => { formData.imageData = file; });
@@ -108,47 +113,49 @@ export class ProfilePage {
             event.preventDefault(); // Предотвращаем стандартное поведение формы (перезагрузку страницы)
 
             try {
+                if (confirm) {
+                    const response = await setUserInfo(formData);
+                    profileForm.reset();
+                    emailInput.addEventListener('input', () => {
+                        if (emailInput.value) {
+                            formData.email = emailInput.value;
+                        }
+                    });
+                    passwordInput.addEventListener('input', () => {
+                        if (passwordInput.value && validatePassword(passwordInput.value) === '') {
+                            formData.password = Array.from(new TextEncoder().encode(passwordInput.value));
+                        } else {
+                            new Notify(validatePassword(passwordInput.value));
+                        }
+                    });
 
-                const response = await setUserInfo(formData);
-                profileForm.reset();
-                emailInput.addEventListener('change', () => {
-                    if (emailInput.value) {
-                        formData.email = emailInput.value;
-                    }
-                });
-                passwordInput.addEventListener('change', () => {
-                    if (passwordInput.value) {
-                        formData.password = Array.from(new TextEncoder().encode(passwordInput.value));
-                    }
-                });
-                // // fileInput.addEventListener('change', () => { formData.imageData = file; });
-                // formData = {
-                //     id: Number(localStorage.getItem('userId')),
-                // };
-                if (response.ok) {
-                    // Обработка успешного ответаArray.from(uint8Array)
-                    // Обработка ошибки
-                    new Notify('Профиль успешно обновлен');
-                    store.dispatch($sentUserInfoRequest());
+                    if (response.ok) {
+                        // Обработка успешного ответаArray.from(uint8Array)
+                        // Обработка ошибки
+                        new Notify('Профиль успешно обновлен');
+                        store.dispatch($sentUserInfoRequest());
 
-                    /*if (document.querySelector('iframe')) {
-                        document.querySelector('iframe').remove();
+                        /*if (document.querySelector('iframe')) {
+                            document.querySelector('iframe').remove();
+                        }
+                        const access = await getCheckSurvey('csi_profile');
+                        console.info(access.body.passed)
+                        if (access.body.passed == false) {
+                            console.info("test")
+                            const frame = document.createElement('iframe');
+                            frame.width = '889';
+                            frame.height = '500';
+                            frame.src = 'http://localhost:81/csi_profile';
+                            frame.frameBorder = '0';
+                            frame.allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share';
+                            frame.allowFullscreen = true;
+                            document.body.appendChild(frame);
+                        }*/
                     }
-                    const access = await getCheckSurvey('csi_profile');
-                    console.info(access.body.passed)
-                    if (access.body.passed == false) {
-                        console.info("test")
-                        const frame = document.createElement('iframe');
-                        frame.width = '889';
-                        frame.height = '500';
-                        frame.src = 'http://localhost:81/csi_profile';
-                        frame.frameBorder = '0';
-                        frame.allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share';
-                        frame.allowFullscreen = true;
-
-                        document.body.appendChild(frame);
-                    }*/
+                }else{
+                    new Notify('Что то вы вводите не так:)');
                 }
+
             } catch (error) {
                 new Notify('Произошла ошибка при отправке запроса');
             }
