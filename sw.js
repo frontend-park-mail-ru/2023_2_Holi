e5ccf2189af4b8958d5d9f74f20d5f28f601d7ce
Alexey <ax.chinaev@yandex.ru>
2023-11-30 10:29:46 +0300

    sw withou wb

--- sw.js
+++ sw.js
@@ -1,29 +1,33 @@
-import { precacheAndRoute } from 'workbox-precaching';
 
-const CACHE_NAME = 'offline-v1';
-precacheAndRoute(self.__WB_MANIFEST);
-/*const PRECACHED = [
-    'src/static/img/netflix.svg',
-    // 'static/wednesday.webm',
-];
+const CACHE = 'offline-v1';
 
-self.addEventListener('install', (event) => {
-    event.waitUntil(caches.open(CACHE_NAME).then((cache) => {
-        return cache.addAll(PRECACHED);
-    }));
-});*/
+// self.addEventListener('activate', function(event) {
+//     event.waitUntil(
+//       caches.keys().then((cacheNames) => {
+//         return Promise.all(
+//           cacheNames.map((currentCache) => {
+//             if (currentCache !== CACHE) {
+//               return caches.delete(currentCache);
+//             }
+//           })
+//         );
+//       })
+//     );
+//   });
+
 self.addEventListener('fetch', (event) => {
     if (event.request.headers.get('range')) {
         return;
     }
 
-    event.respondWith(caches.open(CACHE_NAME).then((cache) => {
+    event.respondWith(caches.open(CACHE).then((cache) => {
         return fetch(event.request).then((fetchedResponse) => {
-            cache.put(event.request.url, fetchedResponse.clone()).catch(err => console.info(err));
-
+            if (event.request.method === 'GET') {
+                cache.put(event.request, fetchedResponse.clone()).catch(err => console.info(err));
+            }
             return fetchedResponse;
         }).catch(() => {
-            return cache.match(event.request.url);
+            return cache.match(event.request);
         });
     }));
 });
