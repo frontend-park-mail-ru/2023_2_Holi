e601bf14fbbfba029257d3f6d572827d3507e97b
Alexey <ax.chinaev@yandex.ru>
2023-12-01 11:37:28 +0300

    fix urls

--- src/services/api/const.js
+++ src/services/api/const.js
@@ -1,2 +1,2 @@
-export const NETFLIX_AUTH_API = 'http://localhost:81/api/v1';
-export const NETFLIX_API = 'http://localhost:81/api/v1';
+export const NETFLIX_AUTH_API = '/api/v1';
+export const NETFLIX_API = '/api/v1';
