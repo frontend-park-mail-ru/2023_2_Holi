4a07632a99784b12895b99c3673615f4b3e9afe2
maffin1799 <nik.chujko@mail.ru>
2023-12-20 10:24:19 +0300

    fx

--- src/pages/serials/serials-feed/serial-content.js
+++ src/pages/serials/serials-feed/serial-content.js
@@ -50,7 +50,7 @@ export class SerialContentPage {
 
         const video = document.querySelector('video');
         video.load();
-        video.addEventListener('loadedmetadata', function () {
+        video.addEventListener('loadedmetadata', function() {
             const durationInSeconds = video.duration;
 
             // Преобразуем длительность из секунд в часы и минуты
